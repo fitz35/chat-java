cb13921b517f6e3e10798aeeb9a4505c222d09e7
Tushita Ramkarn <tushitaramkaran@gmail.com>
2021-11-18 14:55:52 +0100

    add connection notif

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -24,8 +24,10 @@ public class ServerThread extends Thread
     {
         try {
             BufferedReader socIn = null;
+            PrintStream socOut = null;
             socIn = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
+            socOut= new PrintStream(clientSocket.getOutputStream());
             String line= socIn.readLine();
             String[] details= line.split("/");
             String roomName= details[1];
@@ -34,13 +36,15 @@ public class ServerThread extends Thread
             if(!clientDoesNotExist)
             {
                 System.out.println("Client exists already in this room");
+                socOut.println("/Client exists already in this room/");
 
             }
             else
             {
                 ClientBack client= new ClientBack(details[0], clientSocket);
                 room.addClient(client);
-                System.out.println("Connexion from:" + clientSocket.getInetAddress()+ "called"+details[0]);
+                System.out.println("Connexion from: " + clientSocket.getInetAddress()+ " called "+details[0]);
+                socOut.println(" /Connexion /");
                 line= socIn.readLine();
                 while(line. compareTo("exit")!=0)
                 {
